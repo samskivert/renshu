9ede40e454eefd3d876f464c655b4cecaa3604fe
Michael Bayne <mdb@samskivert.com>
2019-05-08 09:35:21 -0700

    Prune some pinned tabs stuff.
    Not sure I'm going to implement that. There's still some cruft in stores, but
    I'll leave that for now.

--- src/app.tsx
+++ src/app.tsx
@@ -173,10 +173,6 @@ const TabInfo :TabData[] = [
   // {tab: "perfs",    title: "Performances", icon: <UI.Icon name={V.Icons.Perf} />},
   {tab: "about",    title: "About",        icon: <UI.Icon name={V.Icons.About} />},
 ]
-// function infoFor (tab :S.Tab) :TabData {
-//   for (let info of TabInfo) if (info.tab === tab) return info
-//   return TabInfo[0]
-// }
 
 @observer
 export class AppView extends React.Component<{store :S.AppStore}> {
@@ -222,53 +218,5 @@ export class AppView extends React.Component<{store :S.AppStore}> {
         {content}
       </div>
     </div>
-
-    // function appView (stores :S.Stores, tab :S.Tab, toolbar :JSX.Element) :JSX.Element {
-    //   const [content, footer] = contentView(store, stores, width, tab)
-    //   return <div key={tab} className={classes.section}>
-    //     <UI.AppBar className={classes.appBar}>{toolbar}</UI.AppBar>
-    //     <main className={classes.content}>{content}</main>
-    //     <UI.AppBar color="secondary" className={classes.appBar}>{footer}</UI.AppBar>
-    //   </div>
-    // }
-    // function auxView (stores :S.Stores, info :TabData) :JSX.Element {
-    //   const {tab, icon, title} = info, unpin = () => store.unpin(tab)
-    //   const footer = <UI.Toolbar>
-    //     <UI.IconButton color="inherit">{icon}</UI.IconButton>
-    //     <UI.Typography style={{marginRight: 5}} variant="h6" color="inherit">{title}</UI.Typography>
-    //     <V.Spacer />
-    //     <UI.IconButton color="inherit" onClick={unpin}><Icons.Close /></UI.IconButton>
-    //   </UI.Toolbar>
-    //   return appView(stores, tab, footer)
-    // }
-
-    // const hideLogoff = () => store.showLogoff = false
-    // const logoff = () => { firebase.auth().signOut() ; hideLogoff() }
-    // const logoffDialog =
-    //   <UI.Dialog open={store.showLogoff} onClose={hideLogoff} aria-labelledby="logoff-title">
-    //     <UI.DialogTitle id="logoff-title">{"Sign out?"}</UI.DialogTitle>
-    //     <UI.DialogActions>
-    //       <UI.Button onClick={hideLogoff} color="primary">No</UI.Button>
-    //       <UI.Button onClick={logoff} color="primary" autoFocus>Yes</UI.Button>
-    //     </UI.DialogActions>
-    //   </UI.Dialog>
-
-    // const mainToolbar = <UI.Toolbar>
-    //   <UI.Typography style={{marginRight: 5}} variant="h6" color="inherit">I/O</UI.Typography>
-    //   {TabInfo.filter(info => !store.isPinned(info.tab))
-    //           .map(info => menuButton(info.tab, info.icon, () => store.tab = info.tab))}
-    //   <V.Spacer />
-    //   {width !== "xs" && menuButton("pin", Icons.pin, () => store.pin(store.tab))}
-    //   {menuButton("logoff", <Icons.CloudOff />, () => store.showLogoff = true)}
-    //   {logoffDialog}
-    // </UI.Toolbar>
-
-    // if (store.pinned.length > 0) return (
-    //   <div className={classes.panes}>
-    //   {appView(stores, store.tab, mainToolbar)}
-    //   {store.pinned.map(tab => auxView(stores, infoFor(tab)))}
-    //   </div>
-    // )
-    // else return appView(stores, store.tab, mainToolbar)
   }
 }
